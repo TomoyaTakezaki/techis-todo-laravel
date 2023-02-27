6f3718f66086e1d36420cec9bdd5b98ce62182c1
TomoyaTakezaki <118738702+TomoyaTakezaki@users.noreply.github.com>
2023-02-26 21:39:35 -0600

    change migration

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,11 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('personal_access_tokens');
+        
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->charset("utf8")->unique();
+            $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
